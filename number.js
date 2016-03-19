144f3a475cfd195705391a9b30f75bc8b9b6ce39
IbraDirir <khaliildirir@gmail.com>
2016-03-19 11:31:34 +0300

    It worked, the programme now generates RandomNumbers from 1 to what the user inserted

--- number.js
+++ number.js
@@ -1,1 +1,9 @@
+//define a variable
+//store prompt in the variable 
+//parse to Int whatever the user inserts and make the number random *  // whatever the user inserted and floor it down.
+//then alert the user with what he entered
+
 var number = prompt("Please put a number");
+var InsertedNumber = parseInt(number);
+var RandomedNumber = Math.floor(Math.random() * InsertedNumber + 1);
+document.write(RandomedNumber);
