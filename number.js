009a92ff0203606be03fcfba4421701ee30efabe
IbraDirir <khaliildirir@gmail.com>
2016-03-20 06:55:50 +0300

    prompting the user twice, the programme generates a number between the two values typed

--- number.js
+++ number.js
@@ -2,8 +2,9 @@
 //store prompt in the variable 
 //parse to Int whatever the user inserts and make the number random *  // whatever the user inserted and floor it down.
 //then alert the user with what he entered
-
+var input1 = prompt("Please input starting number");
+var FirstNumber = parseInt(input1);
 var number = prompt("Please put a number");
 var InsertedNumber = parseInt(number);
-var RandomedNumber = Math.floor(Math.random() * InsertedNumber + 1);
-document.write(RandomedNumber);
+var RandomedNumber = Math.floor(Math.random() * (InsertedNumber - FirstNumber)) + FirstNumber;
+alert(RandomedNumber);
